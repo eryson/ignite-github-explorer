6b9727b6518bb040153e56b076734b39bc841fe4
eryson <eryson.ktm@gmail.com>
2021-09-26 17:46:04 -0300

    dev: Created first component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+const repositorName = "unform2";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositorName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositórios</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositórios</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositórios</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
