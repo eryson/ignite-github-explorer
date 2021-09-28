c0978c38ef3b7663065378437f5524eecfc56d2f
Eryson Miranda <eryson.ktm@gmail.com>
2021-09-28 10:14:31 -0300

    dev: added data fetching in github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
+
+// https://api.github.com/users/eryson/repos
 
 const repository = {
   name: "unform",
@@ -9,6 +12,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/eryson/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
