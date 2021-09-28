4ef9e09084371e4fe98083f7ecfaf4efde8f80d4
Eryson Miranda <eryson.ktm@gmail.com>
2021-09-28 10:21:33 -0300

    feat: added repository listing

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
-// https://api.github.com/users/eryson/repos
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/eryson/ignite-github-explorer",
-};
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -25,9 +17,11 @@ export function RepositoryList() {
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
